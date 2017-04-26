c176ad60cc2eb792082ef7b723413c735352893d
Lemurrhino <lemur1997@outlook.com>
2017-04-26 15:43:51 -0400

    Added Things for travis-ci

--- bot.py
+++ bot.py
@@ -1,9 +1,10 @@
+
 import discord
 from discord.ext import commands
 import logging
-import ConfigParser
+import configparser
 
-configParser = ConfigParser.RawConfigParser()
+configParser = configparser.RawConfigParser()
 configFilePath = r'c:\abc.txt'
 configParser.read(configFilePath)
 
